f210bc386e015a091bb987df83cae76cbdd3dac8
Vinicius Nadin <vininadin@gmail.com>
2023-06-15 09:58:01 -0300

    Create the FeedItem model

---
+++ ShowSpotFeed/ShowSpotFeed/FeedItem.swift
@@ -0,0 +1,17 @@
+//
+//  FeedItem.swift
+//  ShowSpotFeed
+//
+//  Created by Vinicius Nadin on 15/06/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let name: String
+    let image: URL
+    let schedule: [String]
+    let genres: [String]
+    let summary: String
+}
