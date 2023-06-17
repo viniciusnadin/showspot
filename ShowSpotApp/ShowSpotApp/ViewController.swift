090362d43343692b6b1eb1fa3a95b8ede56adbcf
Vinicius Nadin <vininadin@gmail.com>
2023-06-16 23:22:15 -0300

    Create a workspace to contain all projects

---
+++ ShowSpotApp/ShowSpotApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShowSpotApp
+//
+//  Created by Vinicius Nadin on 16/06/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
